0e9a6c2eafb8478c1162337cf26b01f8629fb5ee
shinopai <harbor.shinozaki@gmail.com>
2022-07-28 17:08:07 +0900

    complete

---
+++ asset/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+  // ページ内リンク
+
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top;
+    var speed = 500;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      speed,
+      "swing"
+    );
+    return false;
+  });
+});
